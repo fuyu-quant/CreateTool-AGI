340ed643d567574db2c079e8731d7303b480c65d
toma tanaka <ulti4929@gmail.com>
2023-05-14 00:20:44 +0900

    a

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+#def requirements_from_file(file_name):
+#    return open(file_name).read().splitlines()
+
+
+setup(
+    name='createtoolagi',
+    version='0.0.1',
+    description="createtoolagi",
+    author='fuyu-quant',  
+    packages=find_packages(),
+    license='MIT'
+)
