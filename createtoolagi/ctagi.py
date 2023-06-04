548caafcc8a6a8c98111d70eee238bebd1589bfb
toma tanaka <ulti4929@gmail.com>
2023-06-04 19:07:31 +0000

    update

--- createtoolagi/ctagi.py
+++ createtoolagi/ctagi.py
@@ -2,6 +2,7 @@ from langchain.llms import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 
 from qdrant_client import QdrantClient
+from qdrant_client import models
 
 from .creator import Creator
 from .decider import Decider
