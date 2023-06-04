9c03c9e2a2500191336a1e761591c63b328ca08d
toma tanaka <ulti4929@gmail.com>
2023-06-04 16:16:14 +0000

    update

---
+++ createtoolagi/creator.py
@@ -0,0 +1,55 @@
+from langchain.llms import OpenAI
+import re
+
+
+class Creater():
+    def __init__(
+        self,
+        model,
+        ):
+        self.create_model = create_model
+
+    def run(self, input, search_result, created_tool_code):
+        print("> Create a tool.")
+
+        related_tools = ''
+        for i in range(len(search_result)):
+            disc = search_result[i].payload['discription']
+            code = search_result[i].payload['code']
+            
+        related_tools += 'discription:' + '\n' + disc +'\n' + 'code:' +'\n' + code + '\n' + '------------------' + '\n'
+
+
+
+        create_prompt = """
+        Please create your code in compliance with all of the following conditions.
+        ・Create a python class that can execute {input_} with a single string as input.
+        ・Output should be code only.
+        ・Do not enclose the output in ``python ``` or the like.
+        ・from langchain.tools import BaseTool must be written.
+        ・Class must inherit from BaseTool.
+        ・If you have previously created code that failed to execute, please refer to it as well.
+        ・Here is the code I created previously: {created_tool_code_}
+        
+        ------------------
+        {related_tools_}
+        """.format(
+            input_ = input, 
+            created_tool_code_ = created_tool_code, 
+            related_tools_ = related_tools
+            )
+
+        code = self.create_model(create_prompt)
+
+        tool_name = re.search(r'name = "(.*?)"', code).group(1)
+
+
+        print('\033[32m' + 'Completed!')
+        print('Created tool name：' + tool_name + '\n' + '\033[0m')
+
+        # Save to File
+        #if folder_path_ != None:
+        #    with open(folder_path_ + f'{name}.py', mode='w') as file:
+        #        file.write(code)
+
+        return code
---
+++ createtoolagi/ctagi.py
@@ -0,0 +1,67 @@
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+from qdrant_client import QdrantClient
+
+from creator import Creater
+from decider import Decider
+from executor import Executor
+from searcher import Searcher
+
+
+class CTAGI():
+    def __init__(
+        self,
+        base_model_name,
+        create_model_name,
+        embegging_model_name,
+        ):
+        self.base_model_name = base_model_name
+        self.base_model = OpenAI(temperature=0,model_name = self.base_model_name)
+
+        self.create_model_name = create_model_name
+        self.create_model = OpenAI(temperature=0,model_name = self.create_model_name)
+
+        self.embegging_model_name = embegging_model_name
+        self.embegging_model = OpenAIEmbeddings(model = self.embegging_model_name)
+
+        self.input = input
+
+        self.qdrant = QdrantClient(path='../tools')
+
+        #self.planner = Planner(model = self.model_name, input = self.input)
+        self.searcher = Searcher(embedding_model = self.embegging_model, qdrant = self.qdrant)
+        self.decider = Decider(base_model = self.base_model)
+        self.creater = Creater(create_model = self.create_model)
+        self.executor = Executor(base_model = self.base_model)
+
+    def run(self, input):
+        search_result = self.searcher.run(input)
+
+        decision = self.decider.run(input, search_result = search_result)
+
+        if decision == "Yes.":
+            self.executor.run(input, search_result = search_result)
+
+        elif decision == "No.":
+            count = 0
+            created_tool_code = None
+            while count < 5:
+                count += 1
+                print(f'Try:{count}')
+                try:
+                    tool_code = self.creater(input, search_result = search_result, created_tool_code = created_tool_code)
+                    created_tool_code = tool_code
+            
+                    self.executor.run_with_create_tool(input, search_result = search_result, tool_code = tool_code)
+                    
+                    self.searcher.save(tool_code = tool_code)
+                    break
+                except Exception as e:
+                    print('\033[32m' + f"Error occurred: {e}" + '\n' + '\033[0m')
+                    
+            if count >= 5:
+                print('\033[32m' + "Reached the maximum number of tries." + '\033[0m')
+
+            
+        return output
