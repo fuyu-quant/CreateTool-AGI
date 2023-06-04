ac470367e4b9e18e3091a6bc30b04363494f6c08
toma tanaka <ulti4929@gmail.com>
2023-06-04 17:48:34 +0000

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='createtoolagi',
-    version='0.0.5',
+    version='0.1.1',
     description="createtoolagi",
     author='fuyu-quant',  
     packages=find_packages(),
